dcfb701f917fd53fab171dea4dee8896f9069265
ShaneDsouza4 <shane.json96@gmail.com>
2025-05-24 18:56:55 -0400

    Adjusted reducer for effects

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,11 +1,18 @@
 import { createReducer, on } from "@ngrx/store";
 import { Book } from "../models/book";
-import { AddBook, RemoveBook } from "./book.action";
+import { AddBook, AddBookFailure, AddBookSuccess, RemoveBook } from "./book.action";
 
 export const initialState: Book[] = [];
 
+//Add Book action was dispatched, and communicatio with actions to see if observable was success or not
 export const BookReducer = createReducer(
     initialState,
-    on(AddBook, (state, { id, title, author }) => [...state, { id, title, author }]),
-    on(RemoveBook, (state, { bookId }) => state.filter(book => book.id != bookId))
+    on(AddBook, (state) => { return state }), //Initial State Action does not need to update state
+    on(AddBookSuccess, (state, { id, title, author }) => [...state, { id, title, author }]), //Was Added Sucesfully
+    on(AddBookFailure, (state, { error }) => { //Error was received, don't need to update state
+        console.error(error);
+        return state;
+    }),
+
+    on(RemoveBook, (state, { bookId }) => state.filter(book => book.id != bookId)),
 )
