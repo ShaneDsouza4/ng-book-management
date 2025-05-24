ce1d45c19ba00e86bfc666159aa91088359ac737
ShaneDsouza4 <shane.json96@gmail.com>
2025-05-24 18:15:15 -0400

    Loaded books from the store using Observable

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { AddBook, RemoveBook } from '../books/book.action';
 import { Book } from '../models/book';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book-list',
@@ -10,9 +11,13 @@ import { Book } from '../models/book';
 })
 export class BookListComponent {
 
+  books$: Observable<Book[]>;
+
   constructor(
     private _store: Store<{ books: Book[] }>
-  ) { }
+  ) {
+    this.books$ = _store.pipe(select('books')); //Select books from store
+  }
 
   addBook(id: string, title: string, author: string) {
     this._store.dispatch(AddBook({ id, title, author }))
